3aae9783569b53cb3e192c2468c8e699db11b67c
gokesolomon <gokehsolomon@gmail.com>
2024-03-28 14:29:13 -0700

    developing

--- Harmony-/HAMONYDEMO/asgi.py
+++ Harmony-/HAMONYDEMO/asgi.py
@@ -1,7 +1,8 @@
 """
 ASGI config for HAMONYDEMO project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It will expose the ASGI callable as a module-level
+variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
