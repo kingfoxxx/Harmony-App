77437297976a3d7a43a0339631872408ebe84fc0
kingfoxxx <brightally6@gmail.com>
2024-03-29 03:37:03 +0100

    updating forms.py

--- Harmony-/Harmony/forms.py
+++ Harmony-/Harmony/forms.py
@@ -4,6 +4,7 @@ from .models import CustomUser
 class SignUpForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
+#we implement our custum authentication fields for users using specific details
     class Meta:
         model = CustomUser
         fields = ['email', 'phone_number', 'username', 'password']
